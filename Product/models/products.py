55f0428b027443f1574a7db14fc4398d80307ee3
Yacn1414 <yacn1214@gmail.com>
2023-08-04 16:25:27 +0330

    new update!

--- Product/models/products.py
+++ Product/models/products.py
@@ -23,16 +23,6 @@ class IpAddress(models.Model):
 
 
 class Product(models.Model):
-    LANG= [
-        ("Fa","فارسی"),
-        ("En","انگلیسی")
-    ]
-    TYPE_TUTORIAL = [
-        ('documnet','documnet'),
-        ('video','video'),
-        ('short video','short video'),
-        ('package','package'),
-    ]
     LEVEL_TUTORIAL = [
     ('مبتدی','مبتدی'),
     ('متوسط','متوسط'),
@@ -42,16 +32,14 @@ class Product(models.Model):
     slug = models.SlugField(max_length=100, verbose_name="سلاگ")
     teacher_name = models.ForeignKey(teachers,on_delete=models.SET_NULL,blank=True,null=True)
     image = models.ImageField(upload_to="images/products", verbose_name="تصویر پیشنمایش")
-    tutorial = models.CharField(max_length=100, verbose_name="نوع دوره",choices=TYPE_TUTORIAL,default="video")
+    tutorial = models.CharField(max_length=100, verbose_name="نوع دوره",default="pdf")
     tutorial_level = models.CharField(max_length=100, verbose_name="سطح دوره",choices=LEVEL_TUTORIAL,default="junior")
-    tutorial_count = models.IntegerField("تعداد ویديو",default=0)
     small_description = models.TextField(verbose_name="توضیح کوتاه")
     long_description = models.TextField(verbose_name="توضیح مفصل")
     price = models.IntegerField(verbose_name="قیمت",default=0)
     pricepercent = models.IntegerField(verbose_name="درصد تخفیف", default=0)
     hot_price = models.IntegerField(verbose_name="قیمت بگایی تخفیف", default=0)
     language = models.ManyToManyField(Languages,verbose_name="زبان برنامه نویسی")
-    tpye_lang = models.CharField(max_length=2,choices=LANG ,default="Fa",verbose_name="زبان محصول") # persian or english
     student_count = models.IntegerField(default=0,verbose_name="تعداد دانشجو")
     published = models.BooleanField(default=True,verbose_name="وضعیت")
     resualt = models.TextField("به چه سطحی میرسه بعد دیدن؟",null=True,blank=True)
