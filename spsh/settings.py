028a4197c65f35594d2a913e497497dd3a51be91
Yacn1414 <yacn1214@gmail.com>
2024-06-30 17:07:31 -0400

    add sqlite

--- spsh/settings.py
+++ spsh/settings.py
@@ -126,20 +126,25 @@ WSGI_APPLICATION = 'spsh.wsgi.application'
 #         'NAME': BASE_DIR / 'data/db.sqlite3',
 #     }
 # }
+# DATABASES = {
+# 'default': {
+# 'ENGINE': 'django.db.backends.mysql',
+# 'NAME': 'django',
+# 'USER': 'yacn1',
+# 'PASSWORD': 'password',
+# 'HOST': 'localhost',
+# 'PORT': '3306',
+# 'OPTIONS': {
+# 'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+# }
+# }
+# }
 DATABASES = {
-'default': {
-'ENGINE': 'django.db.backends.mysql',
-'NAME': 'django',
-'USER': 'root',
-'PASSWORD': '',
-'HOST': 'localhost',
-'PORT': '3306',
-'OPTIONS': {
-'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
 }
-}
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
